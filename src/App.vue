3332c9ae927e1603fe38b13ea7b03bbf9e033c53
kchin <kchin2gl@gmail.com>
2024-10-25 15:06:23 +0800

    feat: element-plus config

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue'
 </script>
 
 <template>
   <div class="box">
-    <h1>test</h1>
+    <el-button type="primary" :icon="Edit">按钮</el-button>
   </div>
 </template>
 
 <style scoped scss>
-.box {
-  width: 300px;
-  height: 600px;
-  h1 {
-    color: pink;
-  }
-}
 </style>

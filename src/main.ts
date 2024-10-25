a45056fcc5c14c6953b83aa20eb60e08ec7fa94f
kchin <kchin2gl@gmail.com>
2024-10-25 15:20:44 +0800

    feat: test husky

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 const app = createApp(App)
 app.use(ElementPlus, {
+    
     locale: zhCn
 })
 app.mount('#app')

e900a45c1cd08e5ad810bd8153364ef1127ac691
kchin <kchin2gl@gmail.com>
2024-11-09 15:41:46 +0800

    feat: scss配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/styles/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)

ad364c593aff085701c72a4c90fab1901815d7a7
kchin <kchin2gl@gmail.com>
2024-11-09 14:37:31 +0800

    feat: src别名和环境变量的配置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { Edit } from '@element-plus/icons-vue'
+import Test from '@/components/test.vue'
 </script>
 
 <template>
   <div class="box">
     <el-button type="primary" :icon="Edit">按钮</el-button>
+    <Test></Test>
   </div>
 </template>
 
